001a9134c8e0b7a854544ed2b3d9ed6bbc263e4f
Yazeed Asaad <159422722+YazeedAsaad01@users.noreply.github.com>
2024-02-12 17:10:04 -0800

    Add files via upload

---
+++ array.js
@@ -0,0 +1,41 @@
+let numbers = [
+    10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 
+    11, 22, 33, 44, 55, 66, 77, 88, 99, 110];
+let numbers1 = [
+    783, 149, 920, 432, 715, 587, 264, 975, 685, 378, 
+    190, 839, 256, 512, 64, 991, 706, 372, 623, 308]
+    
+let numbers2 = [
+    492, 136, 897, 781, 322, 960, 543, 278, 614, 72,
+     843, 465, 959, 548, 252, 427, 173, 841, 688, 91]
+    
+let numbers3 = [
+    732, 17, 518, 877, 209, 465, 739, 890, 392, 639, 
+    244, 69, 931, 608, 361, 832, 117, 589, 483, 734]
+    
+let numbers4 = [
+    321, 803, 142, 670, 27, 483, 726, 389, 85, 149, 
+    616, 748, 385, 534, 215, 947, 793, 469, 95, 510]
+    
+let numbers5 = [
+    672, 501, 238, 108, 764, 637, 140, 811, 956, 73, 
+    628, 354, 121, 402, 571, 489, 820, 228, 996, 663]
+    
+
+    function find(array) {
+        let bigger = array[0];
+
+        for(let i = 0; i < array.length; i++) {
+            if(array[i] > bigger) {
+               bigger = array[i]
+            }
+        }
+        return bigger;
+    }
+
+    console.log(find(numbers));
+    console.log(find(numbers1));
+    console.log(find(numbers2));
+    console.log(find(numbers3));
+    console.log(find(numbers4));
+    console.log(find(numbers5));
